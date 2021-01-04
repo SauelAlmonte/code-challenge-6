c2c7975b44a746a5ac95387d9e61640df9e174f3
msimbo-coding <SAlmonte@MSIMBO.org>
2021-01-04 00:53:09 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const tableMenu = document.getElementById("book-table")
+
+tableMenu.addEventListener('click', function(){
+  // .show-sidebar
+  // document.body.classList.add('class-3')
+  // document.body.classList.remove('class-2')
+  document.body.classList.toggle('show-sidebar')
+  // tableMenu.classList.add('some-random-class')
+  // console.log(document.body.classList)
+  // console.log(tableMenu.classList)
+})
